a85e08e47a99404943452773cf8f2f116cd77df5
Jefferson Morais <jeffmorais96@gmail.com>
2020-03-03 23:15:20 -0300

    i dunno how to use the .env in angular, i hate myself

--- src/app/core/services/cat.service.ts
+++ src/app/core/services/cat.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { config } from 'src/environments/config';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class CatService {
   constructor(public http: HttpClient) { }
 
   getRandom(): Observable<any> {
-    const headers = new HttpHeaders({ 'x-api-key': process.env.MY_CAT_API_KEY });
+    const headers = new HttpHeaders({ 'x-api-key': config.MY_CAT_API_KEY });
     return this.http.get<any>(`https://api.thecatapi.com/v1/images/search`, { headers });
   }
 }
---
+++ src/environments/config.ts
@@ -0,0 +1,3 @@
+export const config = {
+    MY_CAT_API_KEY: 'e27f8a59-6f43-4290-b0e8-13a5dd00f8bf'
+};
